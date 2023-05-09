b1e12628c712cec29b5c7d07ed08086f05734f8d
Hodges, William D <wdhodges@carilionclinic.org>
2023-05-09 07:38:44 -0400

    fixed an issue that would require more bloat

---
+++ src/Novu/Internal/IsExternalInit.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class IsExternalInit
+    {
+    
+    }   
+}
