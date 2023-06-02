0bced4fa4aecf05f9fc31f31207c868483432775
wh1337 <11448029+wh1337@users.noreply.github.com>
2023-06-02 02:24:59 -0400

    Complete refactor of types

--- src/Novu/NotificationTemplates/Types.cs
+++ src/Novu/NotificationTemplates/Types.cs
@@ -3,70 +3,189 @@ using Newtonsoft.Json.Linq;
 
 namespace Novu.NotificationTemplates;
 
-public record PreferenceSettings(
-    bool Email,
-    bool Sms,
-    bool Chat,
-    bool Push,
-    [property: JsonProperty("in_app")] bool InApp);
+public class PreferenceSettings
+{
+    [JsonProperty("email")]
+    public bool Email { get; set; }
+    
+    [JsonProperty("sms")]
+    public bool Sms { get; set; }
+    
+    [JsonProperty("chat")]
+    public bool Chat { get; set; }
+    
+    [JsonProperty("push")]
+    public bool Push { get; set; }
+    
+    [JsonProperty("in_app")]
+    public bool InApp { get; set; }
+}
 
-public record Child(string Field,
-    string Value,
-    string Operator,
-    string On);
+public class Child
+{
+    [JsonProperty("field")]
+    public string Field { get; set; }
+    
+    [JsonProperty("value")]
+    public string Value { get; set; }
+    
+    [JsonProperty("operator")]
+    public string Operator { get; set; }
+    
+    [JsonProperty("on")]
+    public string On { get; set; }
+}
 
-public record Filter(
-    bool IsNegated,
-    string Type,
-    string Value,
-    Child[] Children);
+public class Filter
+{
+    [JsonProperty("isNegated")]
+    public bool IsNegated { get; set; }
+    
+    [JsonProperty("type")]
+    public string Type { get; set; }
+    
+    [JsonProperty("value")]
+    public string Value { get; set; }
+    
+    [JsonProperty("children")]
+    public Child[] Children { get; set; }
+}
 
-public record Variable(string Name);
+public class Variable
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+}
 
-public record Trigger(
-    string Type,
-    string Identifier,
-    Variable[] Variables,
-    Variable[] SubscriberVariables);
+public class Trigger
+{
+    [JsonProperty("type")]
+    public string Type { get; set; }
+    
+    [JsonProperty("identifier")]
+    public string Identifier { get; set; }
+    
+    [JsonProperty("variables")]
+    public Variable[] Variables { get; set; }
+    
+    [JsonProperty("subscriberVariables")]
+    public Variable[] SubscriberVariables { get; set; }
+}
 
-public record NotificationGroup(
-    [property: JsonProperty("_id")] string Id,
-    [property: JsonProperty("_organizationId")] string OrganizationId,
-    [property: JsonProperty("_environmentId")] string EnvironmentId,
-    [property: JsonProperty("_parentId")] string ParentId,
-    string Name);
+public class NotificationGroup
+{
+    [JsonProperty("_id")]
+    public string Id { get; set; }
+    
+    [JsonProperty("_organizationId")]
+    public string OrganizationId { get; set; }
+    
+    [JsonProperty("_environmentId")]
+    public string EnvironmentId { get; set; }
+    
+    [JsonProperty("_parentId")]
+    public string ParentId { get; set; }
+    
+    [JsonProperty("name")]
+    public string Name { get; set; }
+}
 
-public record Step(
-    [property: JsonProperty("_id")]string Id,
-    string Name,
-    [property: JsonProperty("_templateId")]string TemplateId,
-    bool Active,
-    bool ShouldStopOnFail,
-    object Template,
-    Filter[] Filters,
-    [property: JsonProperty("_parentId")]JObject ParentId,
-    JObject Metadata,
-    JObject ReplyCallback); // TODO: Metadata is a JObject, but it can be anything. Need to figure out how to handle this.
+public class Step
+{
+    [JsonProperty("_id")]
+    public string Id { get; set; }
+    
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonProperty("_templateId")]
+    public string TemplateId { get; set; }
+    
+    [JsonProperty("active")]
+    public bool Active { get; set; }
+    
+    [JsonProperty("shouldStopOnFail")]
+    public bool ShouldStopOnFail { get; set; }
+    
+    [JsonProperty("template")]
+    public object Template { get; set; }
 
-public record NotificationTemplate(
-    [property: JsonProperty("_id")]string Id,
-    [property: JsonProperty("_organizationId")]string OrganizationId,
-    [property: JsonProperty("_creatorId")]string CreatorId,
-    [property: JsonProperty("_environmentId")]string EnvironmentId,
-    [property: JsonProperty("_notificationGroupId")] string NotificationGroupId,
-    [property: JsonProperty("_parentId")] string ParentId,
-    string Name,
-    string Description,
-    bool Active,
-    bool Draft,
-    bool Critical,
-    string[] Tags,
-    PreferenceSettings PreferenceSettings,
-    Step[] Steps,
-    Trigger[] Triggers,
-    bool Deleted,
-    DateTime DeletedAt,
-    string DeletedBy,
-    NotificationGroup NotificationGroup);
+    [JsonProperty("filters")]
+    public Filter[] Filters { get; set; }
+    
+    [JsonProperty("_parentId")]
+    public string ParentId { get; set; }
+    
+    [JsonProperty("metadata")]
+    public object Metadata { get; set; }
+    
+    [JsonProperty("replyCallback")]
+    public object ReplyCallback { get; set; }
+    
+}
 
-public record UpdateTemplateStatusRequest(bool Active);
+public class NotificationTemplate
+{
+    [JsonProperty("_id")]
+    public string Id { get; set; }
+    
+    [JsonProperty("_organizationId")]
+    public string OrganizationId { get; set; }
+    
+    [JsonProperty("_creatorId")]
+    public string CreatorId { get; set; }
+    
+    [JsonProperty("_environmentId")]
+    public string EnvironmentId { get; set; }
+    
+    [JsonProperty("_notificationGroupId")]
+    public string NotificationGroupId { get; set; }
+    
+    [JsonProperty("_parentId")]
+    public string ParentId { get; set; }
+    
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonProperty("description")]
+    public string Description { get; set; }
+    
+    [JsonProperty("active")]
+    public bool Active { get; set; }
+    
+    [JsonProperty("draft")]
+    public bool Draft { get; set; }
+    
+    [JsonProperty("critical")]
+    public bool Critical { get; set; }
+    
+    [JsonProperty("tags")]
+    public string[] Tags { get; set; }
+    
+    [JsonProperty("preferenceSettings")]
+    public PreferenceSettings PreferenceSettings { get; set; }
+    
+    [JsonProperty("steps")]
+    public Step[] Steps { get; set; }
+    
+    [JsonProperty("triggers")]
+    public Trigger[] Triggers { get; set; }
+    
+    [JsonProperty("deleted")]
+    public bool Deleted { get; set; }
+    
+    [JsonProperty("deletedAt")]
+    public DateTime DeletedAt { get; set; }
+    
+    [JsonProperty("deletedBy")]
+    public string DeletedBy { get; set; }
+    
+    [JsonProperty("notificationGroup")]
+    public NotificationGroup NotificationGroup { get; set; }
+}
+
+public class UpdateTemplateStatusRequest
+{
+    [JsonProperty("active")]
+    public bool Active { get; set; }
+}
