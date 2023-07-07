2ccd425756b5468352b5de5b4a48e2aa8446522f
benlin1994 <benlin1994@gmail.com>
2023-07-07 10:03:00 +0200

    fix SubscriberDTO and EventDataTriggerDTO by adding in missing fields

--- src/Novu/DTO/SubscriberDto.cs
+++ src/Novu/DTO/SubscriberDto.cs
@@ -13,11 +13,27 @@ public class SubscriberDto
     [JsonProperty("subscriberId")]
     public string? SubscriberId { get; set; }
     
+    [JsonProperty("email")]
+    public string? Email { get; set; }
+    
     [JsonProperty("firstName")]
     public string? FirstName { get; set; }
-    
+
     [JsonProperty("lastName")]
     public string? LastName { get; set; }
+    
+    [JsonProperty("phone")]
+    public string? Phone { get; set; }
+    
+    [JsonProperty("avatar")]
+    public string? Avatar { get; set; }
+    
+    [JsonProperty("locale")]
+    public string? Locale { get; set; }
+    
+    [JsonProperty("data")]
+    public List<AdditionalDataDto>? Data { get; set; }
+
 
     [JsonProperty("channels")]
     public object[] Channels { get; set; }
