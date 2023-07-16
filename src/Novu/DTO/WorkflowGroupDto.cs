8cac094a48048b720cba3cc81c2293d8da7c6642
Raphael Okeibunor <raphaelnagato19@gmail.com>
2023-07-16 12:10:01 +0100

    Create WorkflowGroup Request and Response Dtos

---
+++ src/Novu/DTO/WorkflowGroupDto.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Novu.DTO
+{
+    public class WorkflowGroupDto
+    {
+        [JsonProperty("name")]
+        public string WorkflowGroupName { get; set; }
+    }
+}
