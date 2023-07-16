cb69eac8e4d6e134e153e36969d43a683b03e333
Raphael Okeibunor <raphaelnagato19@gmail.com>
2023-07-16 13:05:14 +0100

    Add tests for WorkflowGroupClient

---
+++ src/Novu.Tests/WorkflowGroups/WorkflowGroupFixture.cs
@@ -0,0 +1,26 @@
+﻿using Novu.Models;
+
+namespace Novu.Tests.WorkflowGroups
+{
+    public class WorkflowGroupFixture : IDisposable
+    {
+        public NovuClient NovuClient { get; }
+
+        public WorkflowGroupFixture()
+        {
+            var novuConfiguration = new NovuClientConfiguration
+            {
+                ApiKey = Environment.GetEnvironmentVariable("NOVU_API_KEY") ?? throw new InvalidOperationException(),
+            };
+            var novu = new NovuClient(novuConfiguration);
+
+            NovuClient = novu;
+        }
+
+        public async void Dispose()
+        {
+            //Delete resources created
+        }
+
+    }
+}
---
+++ src/Novu.Tests/WorkflowGroups/WorkflowGroupUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using Novu.DTO;
+
+namespace Novu.Tests.WorkflowGroups
+{
+    public class WorkflowGroupUnitTests
+    {
+        public readonly WorkflowGroupFixture _fixture;
+
+        public WorkflowGroupUnitTests(WorkflowGroupFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async void Should_Create_WorkflowGroup()
+        {
+            var client = _fixture.NovuClient;
+
+            var requestBody = new WorkflowGroupDto
+            {
+                WorkflowGroupName = "workflowGroup123"
+            };
+
+            var workflowGroup = await client.WorkflowGroup.CreateWorkflowGroup(requestBody);
+
+            Assert.Equal(requestBody.WorkflowGroupName, workflowGroup.PayloadDto.Name);
+        }
+
+        [Fact]
+        public async void Should_Return_WorkflowGroup_List()
+        {
+            var client = _fixture.NovuClient;
+
+            var listOfWorkflowGroups = await client.WorkflowGroup.GetWorkflowGroups();
+
+            Assert.NotNull(listOfWorkflowGroups);
+            Assert.NotEmpty(listOfWorkflowGroups.PayloadDtos);
+        }
+    }
+}
