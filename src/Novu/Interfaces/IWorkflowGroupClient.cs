f1ff836a93b059f0ad2398887ef1f93667a5f23a
Raphael Okeibunor <raphaelnagato19@gmail.com>
2023-07-16 12:10:50 +0100

    Create Refit interface client for WorkflowGroup

---
+++ src/Novu/Interfaces/IWorkflowGroupClient.cs
@@ -0,0 +1,26 @@
+﻿using Novu.DTO;
+using Refit;
+
+namespace Novu.Interfaces
+{
+    public interface IWorkflowGroupClient
+    {
+        /// <summary>
+        /// Create a Workflow Group
+        /// </summary>
+        /// <param name="requestBody"></param>
+        /// <returns>
+        /// <see cref="WorkflowGroupSingleResponseDto"/> - The created workflow group
+        /// </returns>
+        [Post("/notification-groups")]
+        public Task<WorkflowGroupSingleResponseDto> CreateWorkflowGroup([Body] WorkflowGroupDto requestBody);
+        /// <summary>
+        /// Get All Workflow Groups
+        /// </summary>
+        /// <returns>
+        /// <see cref="WorkflowGroupBulkResponseDto"/> - List of workflow groups
+        /// </returns>
+        [Get("/notification-groups")]
+        public Task<WorkflowGroupBulkResponseDto> GetWorkflowGroups();
+    }
+}
