d44ec509c745a08f87afa8828dd2543996ffdc8b
todd <todd+github@goneopen.com>
2023-08-24 14:00:46 +1200

    refactor: changing naming conventions # 49

---
+++ src/Novu/DTO/WorkflowGroups/WorkflowGroup.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Novu.DTO.WorkflowGroups;
+
+public class WorkflowGroup
+{
+    [JsonProperty("_id")] public string Id { get; set; }
+
+    [JsonProperty("name")] public string Name { get; set; }
+
+    [JsonProperty("_environmentId")] public string EnvironmentId { get; set; }
+
+    [JsonProperty("_organizationId")] public string OrganizationId { get; set; }
+
+    [JsonProperty("_parentId")] public string ParentId { get; set; }
+}
