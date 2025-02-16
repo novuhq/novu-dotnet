ee711cb873e83980aebe3139062ac900607fe77a
toddb <todd-github@goneopen.com>
2025-02-16 17:15:08 +1300

    Merge pull request #93 from novuhq/91-digestregularmetadatabackoffunit-is-serialized-incorrectly-when-not-set
    fix: DigestRegularMetadata.BackoffUnit is serialized incorrectly

--- src/Novu.Domain/Models/Workflows/Step/DigestRegularMetadata.cs
+++ src/Novu.Domain/Models/Workflows/Step/DigestRegularMetadata.cs
@@ -3,37 +3,60 @@ using Newtonsoft.Json;
 
 namespace Novu.Domain.Models.Workflows.Step;
 
+/// <summary>
+///     Represents the metadata for a regular digest step in a workflow.
+/// </summary>
+/// <remarks>
+///     - The <see cref="Type" /> property determines whether this metadata applies to a regular digest or a backoff-based
+///     digest.
+///     - The <see cref="Backoff" /> property controls whether backoff logic is enabled.
+///     - If <see cref="Backoff" /> is true, <see cref="BackoffAmount" /> and <see cref="BackoffUnit" /> determine the
+///     backoff period.
+///     - <see cref="BackoffUnit" /> must always be set to a valid <see cref="UnitEnum" /> value to ensure proper API
+///     compatibility.
+///     - To avoid serialization issues, <see cref="BackoffAmount" /> and <see cref="BackoffUnit" /> are set valid values
+///     to turn off functionality and are serialised with <see cref="BackoffUnit" />
+/// </remarks>
 public class DigestRegularMetadata : DigestBaseMetadata, IWorkflowMetaData
 {
     /// <summary>
-    ///     Allowed <see cref="DigestTypeEnum.Regular" /> and <see cref="DigestTypeEnum.Backoff" />
+    ///     The type of digest, either Regular or Backoff.
     /// </summary>
     /// <remarks>
-    ///     In working with the API <see cref="DigestTypeEnum.Backoff" /> doesn't appear to be used
+    ///     The API currently does not use <see cref="DigestTypeEnum.Backoff" />.
     /// </remarks>
     [Required]
     [JsonProperty("type")]
     public DigestTypeEnum Type { get; set; } = DigestTypeEnum.Regular;
 
     /// <summary>
-    ///     Only if frequent events occur
+    ///     Indicates whether backoff logic is applied.
     /// </summary>
-    [JsonProperty("backoff")]
+    [JsonProperty("backoff", DefaultValueHandling = DefaultValueHandling.Populate)]
     public bool Backoff { get; set; }
 
     /// <summary>
-    ///     More than 1 event in the last amount by unit if <see cref="Backoff" /> true
+    ///     The number of events within the specified <see cref="BackoffUnit" /> to trigger a digest.
     /// </summary>
-    /// <see cref="BackoffUnit" />
-    [JsonProperty("backoffAmount")]
+    /// <remarks>
+    ///     This field is only relevant if <see cref="Backoff" /> is true. 
+    /// </remarks>
+    [JsonProperty("backoffAmount", DefaultValueHandling = DefaultValueHandling.Populate)]
     public int BackoffAmount { get; set; }
 
     /// <summary>
-    ///     More than 1 event in the last amount by unit if <see cref="Backoff" /> true
+    ///     The unit of time for the backoff period.
     /// </summary>
-    /// <see cref="BackoffUnit" />
+    /// <remarks>
+    ///     This field is required by the backend, even if <see cref="Backoff" /> is false.
+    ///     A default value of <see cref="UnitEnum.Seconds" /> is set to prevent serialization errors.
+    /// </remarks>
     [JsonProperty("backoffUnit")]
-    public UnitEnum BackoffUnit { get; set; }
+    public UnitEnum BackoffUnit { get; set; } = UnitEnum.Seconds;
 
-    [JsonProperty("updateMode")] public bool UpdateMode { get; set; }
+    /// <summary>
+    ///     Determines whether update mode is enabled.
+    /// </summary>
+    [JsonProperty("updateMode")]
+    public bool UpdateMode { get; set; }
 }
